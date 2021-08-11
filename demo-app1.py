9a4d80b143d5ba67a3c230f0b4729d996360eb49
pritisatish <86285887+pritisatish@users.noreply.github.com>
2021-08-11 15:33:16 +0530

    Update demo-app1.py

--- demo-app1.py
+++ demo-app1.py
@@ -3,8 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#@app.route('/')
-@app.route('/home')
+@app.route('/')
+#@app.route('/home')
 def home_view():
     return "<h1>Welcome to NSOFT</h1>"
 
